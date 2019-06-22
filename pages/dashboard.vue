0580fdb59fe2d7287f1621e3886279e85748f0a8
Farzeen <happycoder97@protonmail.com>
2019-06-22 07:07:19 +0530

    initial commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <h1 class="title">
+          Dashboard
+        </h1>
+        <h2 class="subtitle">
+          IAME School Parliament 2019
+        </h2>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <button class="button">
+          Administration
+        </button>
+        <button class="button">
+          Voting
+        </button>
+        <button class="button">
+          Results
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  padding-left: 15px;
+  padding-right: 15px;
+  margin: auto;
+  max-width: 400px;
+  margin-top: 30vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <form @submit.prevent>
+      <b-field label="User ID">
+        <b-input v-model="userid" />
+      </b-field>
+      <b-field label="Password">
+        <b-input v-model="password" type="password" password-reveal />
+      </b-field>
+      <button class="button is-primary">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  padding-left: 15px;
+  padding-right: 15px;
+  margin: auto;
+  max-width: 400px;
+  margin-top: 30vh;
+}
+</style>
