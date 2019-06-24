6cc5c1d0c5edcebfd2d2c26cad4c2967466227b3
Farzeen <happycoder97@protonmail.com>
2019-06-25 00:26:45 +0530

    Login done

--- pages/login.vue
+++ pages/login.vue
@@ -1,39 +1,66 @@
 <template>
-  <div>
-    <section class="hero">
-      <div class="hero-body">
-        <h1 class="title">
+  <div class="hero is-light is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-3">
           Login
         </h1>
-        <h2 class="subtitle">
+        <h2 class="subtitle is-3">
           IAME School Parliament 2019
         </h2>
+        <div class="columns">
+          <form class="column is-one-third" @submit.prevent="login">
+            <b-field label="Email">
+              <b-input v-model="creds.email" />
+            </b-field>
+            <b-field label="Password">
+              <b-input v-model="creds.password" type="password" password-reveal />
+            </b-field>
+            <button class="button is-primary">
+              Login
+            </button>
+            <b-notification
+              class="login-notification"
+              type="is-warning"
+              :active.sync="loginFailed"
+            >
+              Login failed. Please check your credentials.
+            </b-notification>
+          </form>
+        </div>
       </div>
-    </section>
-    <section class="container">
-      <form @submit.prevent>
-        <b-field label="User ID">
-          <b-input v-model="userid" />
-        </b-field>
-        <b-field label="Password">
-          <b-input v-model="password" type="password" password-reveal />
-        </b-field>
-        <button class="button is-primary">
-          Login
-        </button>
-      </form>
-    </section>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      loginFailed: false,
+      creds: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login() {
+      const json = await this.$api.login(this.creds)
+      if (json.success) {
+        this.loginFailed = false
+        this.$router.push('/dashboard')
+      } else {
+        this.loginFailed = true
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
-.container {
-  padding-left: 15px;
-  padding-right: 15px;
-  max-width: 400px;
+.login-notification {
+  position: absolute;
+  margin-top: 1em;
 }
 </style>
