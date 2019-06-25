b34aa671d317c8e2dbb7f23e10b3a81ef525e703
Farzeen <happycoder97@protonmail.com>
2019-06-25 19:51:29 +0530

    remove eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,15 +54,15 @@ module.exports = {
      ** You can extend webpack config here
      */
     extend(config, ctx) {
-      config.module.rules.push({
-        enforce: 'pre',
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        exclude: /(node_modules)/,
-        options: {
-          fix: true
-        }
-      });
+      // config.module.rules.push({
+      //   enforce: 'pre',
+      //   test: /\.(js|vue)$/,
+      //   loader: 'eslint-loader',
+      //   exclude: /(node_modules)/,
+      //   options: {
+      //     fix: true
+      //   }
+      // });
       config.node = {
         fs: 'empty'
       }
